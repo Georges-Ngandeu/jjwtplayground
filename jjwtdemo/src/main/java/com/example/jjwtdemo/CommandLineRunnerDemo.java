0cf61da234cac3e8489bfd8853d8e4d8e63b8921
georgesngandeu <georgesngandeu@gmail.com>
2024-03-12 13:46:55 -0400

    just updated some stuff

--- jjwtdemo/src/main/java/com/example/jjwtdemo/CommandLineRunnerDemo.java
+++ jjwtdemo/src/main/java/com/example/jjwtdemo/CommandLineRunnerDemo.java
@@ -5,10 +5,13 @@ import javax.crypto.SecretKey;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import com.example.jjwtdemo.JjwtdemoApplication;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.Password;
 
 @Configuration
 public class CommandLineRunnerDemo {
@@ -20,18 +23,19 @@ public class CommandLineRunnerDemo {
 
             SecretKey key = Jwts.SIG.HS256.key().build();
 
-            String secretString = Encoders.BASE64URL.encode(key.getEncoded());
+            String secretString = Encoders.BASE64.encode(key.getEncoded());
 
             String jws = Jwts.builder().subject("Joe").signWith(key).compact();
 
             System.out.println("key :" + key);
 
-            System.out.println("Jws :" + jws);
-
             System.out.println("secretString :" + secretString);
+
+            System.out.println("Jws :" + jws);
 
             Boolean result = Jwts.parser().verifyWith(key).build().parseSignedClaims(jws).getPayload().getSubject().equals("Joe");
 
-            System.out.println("result :" + result);     };
+            System.out.println("result :" + result);     
+        };
     }
 }
